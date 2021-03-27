9f1780f353799cb280c5f079957b9c686bb87b41
Sergi Puigpinos Palau <sergi.puigpinos@hotmail.com>
2021-03-27 13:47:21 +0100

    Home view and template done

--- Lab_App/urls.py
+++ Lab_App/urls.py
@@ -1,23 +1,23 @@
-from django.contrib.auth.decorators import login_required
-from django.urls import path
 from django.conf.urls import url
-from django.views.generic import DetailView, ListView
-from Lab_App.models import *
+from django.urls import path
+from django.views.generic import ListView, TemplateView
 from Lab_App.views import *
 
-from . import views
 
 app_name = "Lab_App"
 
 urlpatterns = [
-    #path('experiments', views.experiments_page),
+    # path('experiments', views.experiments_page),
 
-    #Experiment List
-    url(r'^experiments/$', ListView.as_view(queryset=Experiment.objects.all().order_by('-init_date'),  #Query to get all experiments
-                         context_object_name='exp_list_obj',  #variable where is stored
-                         template_name='experiment_list.html')),
+    path('', lab_home, name='home'),
 
-    #Experiment details
+    # Experiment List
+    url(r'^experiments/$',
+        ListView.as_view(queryset=Experiment.objects.all().order_by('-init_date'),  # Query to get all experiments
+                         context_object_name='exp_list_obj',  # variable where is stored
+                         template_name='experiment_list.html'), name='experiments'),
+
+    # Experiment details
     url(r'^experiments/(?P<pk>\d+)/$', ExperimentDetail.as_view(), name='experiment_details'),
 
 ]
--- Lab_App/views.py
+++ Lab_App/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.views.generic import DetailView
 
-from .models import Experiment
 
+from Lab_App.models import *
 
 # Create your views here.
 def experiments_page(r):
@@ -15,8 +16,13 @@ class ExperimentDetail(DetailView):
     model = Experiment
     template_name = 'experiment_detail.html'
 
-    def get_context_data(self, **kwargs):
 
-        context = super(ExperimentDetail, self).get_context_data(**kwargs)
-        #context['RATING_CHOICES'] = RestaurantReview.RATING_CHOICES
-        return context
+def lab_home(request):
+    article_list = Article.objects.order_by('-publish_date')
+    scientist_list = Scientist.objects.order_by('-name')
+    template = loader.get_template('laboratory_home.html')
+    context = {
+        'article_list': article_list,
+        'scientist_list': scientist_list,
+    }
+    return HttpResponse(template.render(context, request))
